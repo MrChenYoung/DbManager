2de04624b3f15897e8804f54494bffde4b24cbe1
code007 <yi_aohehe@163.com>
2020-04-26 23:04:29 +0800

    new

---
+++ backup/20200426162551_all_v1.sql
@@ -0,0 +1,197 @@
+--
+-- MySQL database dump
+-- Created by DbManage class, Power By yanue. 
+-- http://yanue.net 
+--
+-- 主机: localhost
+-- 生成日期: 2020 年  04 月 26 日 16:25
+-- MySQL版本: 5.5.5-10.4.8-MariaDB
+-- PHP 版本: 7.3.10
+
+--
+-- 数据库: `movie`
+--
+
+-- -------------------------------------------------------
+
+--
+-- 表的结构movie_list
+--
+
+DROP TABLE IF EXISTS `movie_list`;
+CREATE TABLE `movie_list` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `cover` varchar(64) DEFAULT NULL COMMENT '封面图',
+  `movie_title` varchar(300) DEFAULT NULL COMMENT '标题',
+  `movie_name` varchar(64) DEFAULT NULL COMMENT '视频名',
+  `movie_size` varchar(32) DEFAULT NULL COMMENT '大小',
+  `movie_duration` varchar(32) DEFAULT NULL COMMENT '时长',
+  `click_count` int(11) DEFAULT NULL COMMENT '点击量',
+  `encrypted` tinyint(4) DEFAULT 0 COMMENT '是否已搬运',
+  `isMp4` tinyint(4) DEFAULT 0 COMMENT '是否是mp4',
+  `tags` varchar(32) DEFAULT '' COMMENT '标签',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=77 DEFAULT CHARSET=utf8 COMMENT='视频列表';
+
+--
+-- 转存表中的数据 movie_list
+--
+
+INSERT INTO `movie_list` VALUES('2','5e9978d8a794f.jpg','代客干砲性服务 大岛柚奈','5e9978d8a7949','0 B','00:00:00','0','1','1','3,6,8');
+INSERT INTO `movie_list` VALUES('5','5e99815da66f4.jpg','美臀嘉年华 61','5e99815da66f2','0 B','00:00:00','0','1','1','');
+INSERT INTO `movie_list` VALUES('7','5e998b49930c6.jpg','微醺性爱 紧贴着身体 caribbeancom_032214 566','5e998b49930c0','0 B','00:00:00','0','1','1','');
+INSERT INTO `movie_list` VALUES('8','5e9991cf85112.jpg','【黑丝】好身材清秀黑丝袜紧身白色工作服 超性感让男人们看了忍不住把她强干','5e9991cf8510e','110.94 MB','00:24:19','0','1','0','4,3');
+INSERT INTO `movie_list` VALUES('9','5e99af6e068be.jpg','伊藤优冲击的事情 n0949_yu_ito_','5e99af6e068b8','0 B','00:00:00','0','1','1','');
+INSERT INTO `movie_list` VALUES('11','5e9ac4b217fac.jpg','他人妻味 被鲜肉棒肏到爽翻天 南','5e9ac4b217fa5','576.19 MB','02:03:07','0','1','1','');
+INSERT INTO `movie_list` VALUES('16','5e9bfae1ddc80.jpg','091317_579 老公跟人商量诱奸自','5e9bfae1ddc7c','16.79 MB','','0','1','1','');
+INSERT INTO `movie_list` VALUES('17','5e9c4431899d6','白嫩美乳少妇援交妹捆绑sm啪啪啪','5e9c4431899d6','61.23 MB','00:09:49','0','1','1','');
+INSERT INTO `movie_list` VALUES('18','5e9c452ba3010','MKD S98 护士小姐下了班的兼差 ','5e9c452ba3010','280.07 MB','','0','1','1','');
+INSERT INTO `movie_list` VALUES('20','5e9c61cc44f11','《迷剪》 办公室强暴新秘书之啪啪内射版','5e9c61cc44f11','58.1 MB','00:10:13','0','1','0','');
+INSERT INTO `movie_list` VALUES('36','5e9d5ae6de1ff.jpg','偷偷靠近探访病危老公年轻妻子的危险 佐佐木绘美 caribbeancom_081414 666','5e9d5ae6de1fb','543.24 MB','01:54:32','0','1','1','');
+INSERT INTO `movie_list` VALUES('37','5e9d6c4853bba.jpg','伊藤优冲击的事情 n0949_yu_ito_om','5e9d6c4853bb0','520.09 MB','01:53:08','0','1','1','');
+INSERT INTO `movie_list` VALUES('38','5e9d6edcb384d.jpg','【包养】富豪少爷高价包养了个白富美奶子差点弄破了','5e9d6edcb384a','116.41 MB','00:20:16','0','1','0','');
+INSERT INTO `movie_list` VALUES('39','5e9d6f75d4a05.jpg','(1女2犬）','5e9d6f75d4a03','94.98 MB','00:20:01','0','1','0','');
+INSERT INTO `movie_list` VALUES('40','5e9d700d5cdfd.jpg','【丝袜】与性感高挑黑丝高跟模特玩性虐颜射','5e9d700d5cdfb','123.1 MB','00:37:34','0','1','0','');
+INSERT INTO `movie_list` VALUES('41','5e9d70dfb5783.jpg','【欧美】 破处完又搞3P肛交','5e9d70dfb5780','85.84 MB','00:18:43','0','1','0','');
+INSERT INTO `movie_list` VALUES('42','5e9d7175ba73e.jpg','【成人动漫】淫荡的少女跟各种中年男人做爱，28分钟_1','5e9d7175ba73c','173.88 MB','00:26:56','0','1','0','');
+INSERT INTO `movie_list` VALUES('43','5e9d7262ead20.jpg','【殴羙】身价百万的极品超模的狂野滋味 这面孔和身材简直让人无法抵挡 18分钟','5e9d7262ead1d','94.95 MB','00:18:21','0','1','0','');
+INSERT INTO `movie_list` VALUES('44','5ea39c9b3b961.jpg','按摩棒摩摩擦擦~大福奶讓爹爹們都興奮了！','5ea39c9b3b95d','561.72 MB','02:25:14','0','0','0','');
+INSERT INTO `movie_list` VALUES('45','5ea3c31325151.jpg','黑长直正妹用炮机当暖身！蒙面土豪最后终于使出无套大法！妹妹直接忘情呻吟！','5ea3c3132514e','207.51 MB','00:47:10','0','0','0','');
+INSERT INTO `movie_list` VALUES('46','5ea3d3533c16a.jpg','想结婚的！主播头戴白纱和小哥哥激情浪操~','5ea3d3533c167','515.84 MB','01:14:05','0','0','0','');
+INSERT INTO `movie_list` VALUES('47','5ea3ef0d3d2b5.jpg','国产AV 猥瑣爸爸激戰班主任！告狀不成反被上！','5ea3ef0d3d2b2','163.61 MB','00:23:45','0','0','0','');
+INSERT INTO `movie_list` VALUES('48','5ea3f677f1c1c.jpg','爆乳美女野狼花 狂野刺青E奶妹！猛男激情啪啪！','5ea3f677f1c19','327.45 MB','01:01:14','0','0','0','');
+INSERT INTO `movie_list` VALUES('49','5ea401605a8e4.jpg','国产AV 偷鄰居的內衣被發現？！只好用露出棒棒道歉了！','5ea401605a8e1','172.72 MB','00:22:21','0','0','0','');
+INSERT INTO `movie_list` VALUES('50','5ea405dd99b3f.jpg','聽聲音就知道她很騷! 愛聊色的妹妹只希望哥哥快看: 「下面那麼濕」','5ea405dd99b3d','80.23 MB','00:24:23','0','0','0','');
+INSERT INTO `movie_list` VALUES('51','5ea4087d863d6.jpg','波浪女主播插穴插到开始喷尿！哥哥快来，多抖一点！','5ea4087d863d3','338.12 MB','01:18:32','0','0','0','');
+INSERT INTO `movie_list` VALUES('52','5ea411a7a1994.jpg','【制服诱惑】上小学的小妹被我约到酒店里调教','5ea411a7a1991','109.1 MB','00:25:01','0','0','0','');
+INSERT INTO `movie_list` VALUES('53','5ea41270c3d9e.jpg','【模特】性感美胸三线嫩模酒店大尺度私拍时被摄影师趁机揩油用道具玩弄嫩穴后又故意拍私处,国语!','5ea41270c3d9c','271.54 MB','00:56:06','0','0','0','');
+INSERT INTO `movie_list` VALUES('54','5ea4142b588d3.jpg','【人寿】人与狗 美女教师自 慰被两只狗舔骚B高潮喷出尿','5ea4142b588d0','117.12 MB','00:21:16','0','0','0','');
+INSERT INTO `movie_list` VALUES('55','5ea414e4940ee.jpg','【国产】饥渴的情侣，天然G奶卷发妹子猛干肏到爽，姿势也很多','5ea414e4940ea','81.68 MB','00:34:45','0','0','0','');
+INSERT INTO `movie_list` VALUES('56','5ea415d182174.jpg','【潜规则】吉林万昌粮库主任跟会计做爱视频','5ea415d182172','42.91 MB','00:20:36','0','0','0','');
+INSERT INTO `movie_list` VALUES('57','5ea4165676a07.jpg','【超清】女神级别尤物嫩模 酒店与老板性交最新流出','5ea4165676a04','46.5 MB','00:11:06','0','0','0','');
+INSERT INTO `movie_list` VALUES('58','5ea416aa09efd.jpg','【人兽】 狗舒服还是人爽？？','5ea416aa09ef8','33.02 MB','00:09:08','0','0','0','');
+INSERT INTO `movie_list` VALUES('59','5ea416e87afe4.jpg','【角色扮演】红衣服的美国小萝莉被一个穿着狼头的变态男给艹了','5ea416e87afe0','119.53 MB','00:18:28','0','0','0','');
+INSERT INTO `movie_list` VALUES('60','5ea417818a477.jpg','【野战】极品骚女和大雕哥驱车到户外艹B说：好爽啊,干死我的骚B','5ea417818a474','80.09 MB','00:29:14','0','0','0','');
+INSERT INTO `movie_list` VALUES('61','5ea4184d51520.jpg','【群P】被很开放的男友带到朋友家里和他们一起乱交','5ea4184d5151e','84.91 MB','00:28:08','0','0','0','');
+INSERT INTO `movie_list` VALUES('62','5ea4190baf487.jpg','【国产】小姐妹约了2个炮友宾馆直播4P大乱交 还说今天要把男的吸成精尽人亡','5ea4190baf484','86.27 MB','00:31:04','0','0','0','');
+INSERT INTO `movie_list` VALUES('63','5ea419de093c6.jpg','【制服】狂艹制服美女 每一下都要插到底','5ea419de093c3','67.14 MB','00:21:31','0','0','0','');
+INSERT INTO `movie_list` VALUES('64','5ea41a6d9e13e.jpg','【滋味】极品女神黑私滋味让你目不暇接 22分钟','5ea41a6d9e13b','129.08 MB','00:22:27','0','0','0','');
+INSERT INTO `movie_list` VALUES('65','5ea41b31d7cac.jpg','【师生恋】女老师的淫乱性生活','5ea41b31d7ca9','110.75 MB','00:25:37','0','0','0','');
+INSERT INTO `movie_list` VALUES('66','5ea41bf63a9d3.jpg','欧美在女朋友非常不愿意的情况下还是干了她还让她舔精耶21分钟','5ea41bf63a9d1','332.62 MB','00:20:55','0','0','0','');
+INSERT INTO `movie_list` VALUES('67','5ea41d1f427b1.jpg','【殴羙】黑白配','5ea41d1f427ae','113.18 MB','00:35:32','0','0','0','');
+INSERT INTO `movie_list` VALUES('68','5ea41ded2fbeb.jpg','【日本】又白又嫩的学生妹替父亲还债，被两个男人猛插 菊花也不放过','5ea41ded2fbe8','38.54 MB','00:12:17','0','0','0','');
+INSERT INTO `movie_list` VALUES('69','5ea41e3ff11c9.jpg','【国产】大胸少妇逼逼好嫩 操起来特给劲','5ea41e3ff11c5','79.94 MB','00:15:56','0','0','0','');
+INSERT INTO `movie_list` VALUES('70','5ea41eb2bd2df.jpg','91大神KXS上海逛街购物花了不少钱才搞到手的95年越南性感漂亮E奶妹子','5ea41eb2bd2da','124.01 MB','00:28:43','0','0','0','');
+INSERT INTO `movie_list` VALUES('71','5ea41fb0c34b6.jpg','【女仆】萌萌的甜美白丝女仆被单男激情中出','5ea41fb0c34b2','24.37 MB','00:08:17','0','0','0','');
+INSERT INTO `movie_list` VALUES('72','5ea41fde59df9.jpg','【双飞】欧美两个大胖子富婆叫黑人鸭上门服务差一点把黑人给弄死32分钟','5ea41fde59df6','107.31 MB','00:31:51','0','0','0','');
+INSERT INTO `movie_list` VALUES('73','5ea420d37eb35.jpg','【日韓】大胸美女艹起来好爽','5ea420d37eb31','153.52 MB','00:26:21','0','0','0','');
+INSERT INTO `movie_list` VALUES('74','5ea421da5814b.jpg','【女同】 两个丰满姐妹没有男人戴上假J8玩的也是不亦乐乎','5ea421da58148','255.96 MB','00:49:20','0','0','0','');
+INSERT INTO `movie_list` VALUES('75','5ea423671ba8a.jpg','【自卫】极品美女自卫渴望肉棍艹翻他的骚B','5ea423671ba87','30.84 MB','00:10:59','0','0','0','');
+INSERT INTO `movie_list` VALUES('76','5ea53ad0011b4.jpg','浙江豪放留學生，激戰巧克力吉拿棒！穴穴坐著一根，嘴裡也含著一根！','5ea53ad0011b0','121 MB','00:14:26','0','0','0','');
+--
+-- 表的结构movie_resource_path
+--
+
+DROP TABLE IF EXISTS `movie_resource_path`;
+CREATE TABLE `movie_resource_path` (
+  `id` tinyint(4) NOT NULL COMMENT '编号',
+  `type_name` varchar(64) DEFAULT NULL COMMENT '存放的路径类型',
+  `path` varchar(200) DEFAULT NULL COMMENT '路径',
+  `position` varchar(32) DEFAULT NULL COMMENT '视频存储位置',
+  `current_use` tinyint(4) DEFAULT 0 COMMENT '是否正在使用',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='视频资源存储路径表';
+
+--
+-- 转存表中的数据 movie_resource_path
+--
+
+INSERT INTO `movie_resource_path` VALUES('1','thumbnail','/Users/mrchen/Desktop/www/PhpProjects/movieThumbs','local','1');
+INSERT INTO `movie_resource_path` VALUES('2','movie','/Users/mrchen/Desktop/www/PhpProjects/movies','local','1');
+INSERT INTO `movie_resource_path` VALUES('3','thumbnail','/Users/mrchen/Desktop/www/PhpProjects/OneDriver/OneDrive - xKx/movieThumbs','cloudOnline','0');
+INSERT INTO `movie_resource_path` VALUES('4','movie','/Users/mrchen/Desktop/www/PhpProjects/OneDriver/OneDrive - xKx/movies','cloudOnline','0');
+INSERT INTO `movie_resource_path` VALUES('5','thumbnail','D:/MovieProject/thumbnails','local','0');
+INSERT INTO `movie_resource_path` VALUES('6','movie','D:/MovieProject/movies','local','0');
+INSERT INTO `movie_resource_path` VALUES('7','thumbnail','D:/MovieProject/OneDriver/OneDrive - xKx/movieThumbs','cloudOnline','0');
+INSERT INTO `movie_resource_path` VALUES('8','movie','D:/MovieProject/OneDriver/OneDrive - xKx/movies','cloudOnline','0');
+INSERT INTO `movie_resource_path` VALUES('9','thumbnail','I:/PHPProject/movieThumbs','local','0');
+INSERT INTO `movie_resource_path` VALUES('10','movie','I:/PHPProject/movies','local','0');
+INSERT INTO `movie_resource_path` VALUES('11','FFmpeg','/usr/local/bin/ffmpeg','','1');
+INSERT INTO `movie_resource_path` VALUES('12','FFmpeg','C:/Users/yi_ao/Desktop/ffmpeg-20191028-68f623d-win64-static/bin/ffmpeg','','0');
+INSERT INTO `movie_resource_path` VALUES('13','FFmpeg','G:/FFmpeg/bin/ffmpeg','','0');
+INSERT INTO `movie_resource_path` VALUES('14','movie','/Users/mrchen/Desktop/www/PhpProjects/MyMovieOneDriver/OneDrive - xKx/MyMovieData/movies','cloudOnline','1');
+INSERT INTO `movie_resource_path` VALUES('15','thumbnail','/Users/mrchen/Desktop/www/PhpProjects/MyMovieOneDriver/OneDrive - xKx/MyMovieData/movieThumbs','cloudOnline','1');
+--
+-- 表的结构movie_tags
+--
+
+DROP TABLE IF EXISTS `movie_tags`;
+CREATE TABLE `movie_tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '标签编号',
+  `tag_name` varchar(64) DEFAULT NULL COMMENT '标签名称',
+  `movie_count` int(11) DEFAULT 0 COMMENT '视频总量',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COMMENT='视频标签表';
+
+--
+-- 转存表中的数据 movie_tags
+--
+
+INSERT INTO `movie_tags` VALUES('1','国产','-1');
+INSERT INTO `movie_tags` VALUES('2','欧美','0');
+INSERT INTO `movie_tags` VALUES('3','日韩','0');
+INSERT INTO `movie_tags` VALUES('4','制服丝袜','-1');
+INSERT INTO `movie_tags` VALUES('5','情趣内衣','0');
+INSERT INTO `movie_tags` VALUES('6','潮吹','0');
+INSERT INTO `movie_tags` VALUES('7','肛交','0');
+INSERT INTO `movie_tags` VALUES('8','重口','0');
+INSERT INTO `movie_tags` VALUES('9','动漫','0');
+INSERT INTO `movie_tags` VALUES('10','偷拍自拍','0');
+INSERT INTO `movie_tags` VALUES('11','人兽','0');
+--
+-- 表的结构movie_to_download
+--
+
+DROP TABLE IF EXISTS `movie_to_download`;
+CREATE TABLE `movie_to_download` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `cover` varchar(300) DEFAULT NULL COMMENT '视频封面图',
+  `movie_title` varchar(300) DEFAULT NULL COMMENT '视频标题',
+  `movie_m3u8_url` varchar(200) DEFAULT NULL COMMENT '视频播放地址',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=94 DEFAULT CHARSET=utf8 COMMENT='待下载视频列表';
+
+--
+-- 转存表中的数据 movie_to_download
+--
+
+INSERT INTO `movie_to_download` VALUES('89','https://img.hghs.cn:2020/avid5e211c2e59004.jpg',' 絕倫正姊的連擊中出騎乘位幹砲 明里紬','https://cn2.fp73.com/avid5e211c2e59004/index.m3u8');
+INSERT INTO `movie_to_download` VALUES('90','https://img.hghs.cn:2020/3279.jpg','地方妹妹脱衣裸聊~就是要陪你聊骚','https://cn3.fp75.com/avid5dd615138645d/index.m3u8');
+INSERT INTO `movie_to_download` VALUES('91','https://img.hghs.cn:2020/3268.jpg','用自己的纤纤玉手来玩小穴穴','https://cn3.fp75.com/avid5dca1c065cb5c/index.m3u8');
+INSERT INTO `movie_to_download` VALUES('92','https://img.hghs.cn:2020/avid5e5f20beafead.jpg','Tiny4K Jessie Saint Big Dick For Valentines','https://cn4.fp29.com/avid5e5f20beafead/index.m3u8');
+INSERT INTO `movie_to_download` VALUES('93','https://img.hghs.cn:2020/avid5de9d94cc80dc.jpg','EroticaX Natalia Queen Naughty Nurse Natalia','https://cn4.fp29.com/avid5de9d94cc80dc/index.m3u8');
+--
+-- 表的结构movie_tools
+--
+
+DROP TABLE IF EXISTS `movie_tools`;
+CREATE TABLE `movie_tools` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `action` tinyint(4) DEFAULT 0 COMMENT '操作',
+  `flag` varchar(32) DEFAULT NULL COMMENT '功能标识',
+  `func` varchar(200) DEFAULT NULL COMMENT '功能说明',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COMMENT='视频工具表';
+
+--
+-- 转存表中的数据 movie_tools
+--
+
+INSERT INTO `movie_tools` VALUES('0','1','downloadingMovie','正在下载视频标识');
+INSERT INTO `movie_tools` VALUES('2','1','stopDownload','停止下载视频标识');
+INSERT INTO `movie_tools` VALUES('3','0','autoUpdatingMovie','正在自动更新视频标识');
+INSERT INTO `movie_tools` VALUES('4','1','stopAutoUpdateMovie','停止自动更新视频标识');
+INSERT INTO `movie_tools` VALUES('5','0','encryptingMovie','正在加密处理视频标识');
+INSERT INTO `movie_tools` VALUES('6','1','stopEncryptMovie','停止加密处理视频标识');
+INSERT INTO `movie_tools` VALUES('7','0','translatingMp4Movie','正在转换MP4视频标识');
+INSERT INTO `movie_tools` VALUES('8','1','stopTranslatingMp4Movie','停止转换MP4视频标识');
